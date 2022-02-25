1cb5a5b05d1ab2278e4151cfcccc64fe08df40b2
Beliy Nikita <beliy.nikita@uliege.be>
2022-02-25 14:36:12 +0100

    Added crc_generate_log

---
+++ crc_generate_log.m
@@ -0,0 +1,30 @@
+function crc_generate_log(log_path, name)
+  % An utility for copying terminal output to log file
+  % 
+  % Parameters:
+  %   log_path: path to emplacements to log files
+  %   name:     basename of log file to create, supposed to refelect
+  %             the name of programm/script   
+  %
+  % Example:
+  %   generate_log(log_path, 'mpm_Dartell');
+  %   pre_dataset = mpm_Dartell(map_dataset, destination, ...
+  %                             'subjects', args.Results.subjects, ...
+  %                             'name', args.Results.name ...
+  %                             );
+  %
+
+  log_name = fullfile(log_path, [name '.log']);
+  if exist(log_name, 'file')
+    delete(log_name)
+  end
+  diary(log_name);
+
+  len = 30;
+  char = '#';
+  format_str = sprintf('%c %%-%ds%c\n', char, len - 3, char);
+  fprintf('%s\n', pad('', len, char));
+  fprintf(format_str, name);
+  fprintf(format_str, datestr(now(), 'dd/mm/YYYY - HH:MM:SS'));
+  fprintf('%s\n', pad('', len, char));
+end
